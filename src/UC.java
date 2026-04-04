17e4cae5ee492618e74f8a030f1076affd17d063
Prithish Pradhan <pp2193@srmist.edu.in>
2026-04-04 16:09:42 +0530

    UC3

--- src/UC.java
+++ src/UC.java
@@ -1,11 +1,11 @@
 public class UC {
     public static void main(String[] args) {
-        System.out.println(" *****   *****   *****    ***** ");
-        System.out.println("*     * *     * *     *  *      ");
-        System.out.println("*     * *     * *****    *****  ");
-        System.out.println("*     * *     * *             * ");
-        System.out.println("*     * *     * *             * ");
-        System.out.println("*     * *     * *      *      * ");
-        System.out.println(" *****   *****   *       *****  ");
+        System.out.println(String.join("", " ", "*****", "   ", "*****", "   ", "*****", "    ", "*****"));
+        System.out.println(String.join("", "*     *", " ", "*     *", " ", "*     *", "  ", "*      "));
+        System.out.println(String.join("", "*     *", " ", "*     *", " ", "*****", "    ", "*****  "));
+        System.out.println(String.join("", "*     *", " ", "*     *", " ", "*", "             * "));
+        System.out.println(String.join("", "*     *", " ", "*     *", " ", "*", "             * "));
+        System.out.println(String.join("", "*     *", " ", "*     *", " ", "*", "      *      * "));
+        System.out.println(String.join("", " ", "*****", "   ", "*****", "   ", "*", "       *****  "));
     }
 }

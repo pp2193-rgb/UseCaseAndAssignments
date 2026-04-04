3247e98d4bf44b4857f1f0d56f8ca3089886c77f
Prithish Pradhan <pp2193@srmist.edu.in>
2026-04-04 16:18:00 +0530

    UC6

--- src/UC.java
+++ src/UC.java
@@ -1,16 +1,47 @@
 public class UC {
     public static void main(String[] args) {
-        String[] banner = {
-                String.join("", " ", "*****", "   ", "*****", "   ", "*****", "    ", "*****"),
-                String.join("", "*     *", " ", "*     *", " ", "*     *", "  ", "*      "),
-                String.join("", "*     *", " ", "*     *", " ", "*****", "    ", "*****  "),
-                String.join("", "*     *", " ", "*     *", " ", "*", "             * "),
-                String.join("", "*     *", " ", "*     *", " ", "*", "             * "),
-                String.join("", "*     *", " ", "*     *", " ", "*", "      *      * "),
-                String.join("", " ", "*****", "   ", "*****", "   ", "*", "       *****  ")
-        };
+        String[] o = getO();
+        String[] p = getP();
+        String[] s = getS();
+        String[] banner = new String[7];
+        for (int i = 0; i < 7; i++) {
+            banner[i] = String.join("   ", o[i], o[i], p[i], s[i]);
+        }
         for (String line : banner) {
             System.out.println(line);
         }
+    }
+    public static String[] getO() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+    }
+    public static String[] getP() {
+        return new String[]{
+                "***** ",
+                "*    *",
+                "*    *",
+                "***** ",
+                "*     ",
+                "*     ",
+                "*     "
+        };
+    }
+    public static String[] getS() {
+        return new String[]{
+                " *****",
+                "*     ",
+                "*     ",
+                " *****",
+                "      *",
+                "      *",
+                " *****"
+        };
     }
 }
